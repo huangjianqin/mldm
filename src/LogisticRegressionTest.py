e2952f848f393c4053f78a658b947ff5fef22e96
huangjianqin <18814127639@163.com>
2017-06-10 18:40:05 +0800

    初次提交

---
+++ src/LogisticRegressionTest.py
@@ -0,0 +1,29 @@
+# -*- conding: utf-8 -*-
+from pyspark import SparkConf, SparkContext
+from pyspark.mllib.classification import LogisticRegressionWithLBFGS
+from pyspark.mllib.regression import LabeledPoint
+from pyspark.mllib.util import MLUtils
+
+if __name__ == "__main__":
+    conf = SparkConf().setAppName("LogisticRegressionTest").setMaster("local[2]")
+    sc = SparkContext(conf=conf)
+
+    def parsePoint(line):
+        values = [float(x) for x in line.split(' ')]
+        return LabeledPoint(values[0], values[1:])
+
+    # data = sc.textFile("../sample_svm_data.txt")
+    # parsedData = data.map(parsePoint)
+    parsedData = MLUtils.loadLibSVMFile(sc, "../sample_multiclass_classification_data.txt")
+
+    # Build the model
+    model = LogisticRegressionWithLBFGS.train(data=parsedData, iterations=10, numClasses=3)
+
+    # Evaluating the model on training data
+    labelsAndPreds = parsedData.map(lambda p: (p.label, model.predict(p.features)))
+    trainErr = labelsAndPreds.filter(lambda (v, p): v != p).count() / float(parsedData.count())
+
+    sc.stop()
+
+    print("Training Error = " + str(trainErr))
+    # print parsePoint2("1 1:-0.222222 2:0.5 3:-0.762712 4:-0.833333 ")
--- src/SVMTest.py
+++ src/SVMTest.py
@@ -4,7 +4,7 @@ from pyspark.mllib.classification import SVMWithSGD, SVMModel
 from pyspark.mllib.regression import LabeledPoint
 
 if __name__ == "__main__":
-    conf = SparkConf().setAppName("Kmeans").setMaster("local[4]")
+    conf = SparkConf().setAppName("SVMTest").setMaster("local[4]")
     sc = SparkContext(conf=conf)
 
     # Load and parse the data
@@ -17,18 +17,18 @@ if __name__ == "__main__":
     parsedData = data.map(parsePoint)
 
     # Build the model
-    model = SVMWithSGD.train(parsedData, iterations=100)
+    model = SVMWithSGD.train(parsedData, iterations=1000)
 
     # Evaluating the model on training data
     labelsAndPreds = parsedData.map(lambda p: (p.label, model.predict(p.features)))
     trainErr = labelsAndPreds.filter(lambda (v, p): v != p).count() / float(parsedData.count())
 
-    def printR((v, p)):
-        print str(v)
-        print str(p)
-        print "-------------------------"
-
-    labelsAndPreds.foreach(printR)
+    # def printR((v, p)):
+    #     print str(v)
+    #     print str(p)
+    #     print "-------------------------"
+    #
+    # labelsAndPreds.foreach(printR)
 
     sc.stop()
     print("Training Error = " + str(trainErr))
