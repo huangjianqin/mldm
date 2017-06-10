397e56060307c4906bc0cbc3268d8db1bba1a638
huangjianqin <18814127639@163.com>
2017-06-10 15:24:10 +0800

    初次提交

--- src/SVMTest.py
+++ src/SVMTest.py
@@ -22,8 +22,13 @@ if __name__ == "__main__":
     # Evaluating the model on training data
     labelsAndPreds = parsedData.map(lambda p: (p.label, model.predict(p.features)))
     trainErr = labelsAndPreds.filter(lambda (v, p): v != p).count() / float(parsedData.count())
-    print("Training Error = " + str(trainErr))
 
-    # Save and load model
-    model.save(sc, "myModelPath")
-    sameModel = SVMModel.load(sc, "myModelPath")
+    def printR((v, p)):
+        print str(v)
+        print str(p)
+        print "-------------------------"
+
+    labelsAndPreds.foreach(printR)
+
+    sc.stop()
+    print("Training Error = " + str(trainErr))
